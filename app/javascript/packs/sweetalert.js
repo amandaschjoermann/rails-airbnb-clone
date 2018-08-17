51be72a8739eb55823de47cdccf459e12fc9530b
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-17 15:54:48 +0200

    Added a seed and fixed the dashboard a bit

---
+++ app/javascript/packs/sweetalert.js
@@ -0,0 +1,16 @@
+// import swal from 'sweetalert';
+
+// function bindSweetAlertButtonDemo() {
+//   const swalButton = document.getElementById('sweet-alert-demo');
+//   if (swalButton) { // protect other pages
+//     swalButton.addEventListener('click', () => {
+//       swal({
+//         title: "About to delete listing",
+//         text: "Are you sure you want to delete this?",
+//         icon: "error"
+//       });
+//     });
+//   }
+// }
+
+// bindSweetAlertButtonDemo();
